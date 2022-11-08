02948d4f9b87291c366414117428deb8dc8baec8
braincee <kwesi938@gmail.com>
2022-11-08 15:39:42 +0000

    Add methods to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,50 @@
 class RecipesController < ApplicationController
-
     def index
-    end
+     @recipes = Recipe.where(user_id: current_user.id).includes(:recipe_foods)
+ 
+     respond_to do |format|
+       format.html 
+       format.xml  { render :xml => @recipes }
+       format.json { render :json => @recipes }
+     end
+   end
+ 
+   def show
+     @recipe = Recipe.where(user_id: current_user.id).find(params[:id]).includes(:recipe_foods)
+   end
+ 
+   def new
+     @recipe = Recipe.new
+     respond_to do |format|
+       format.html { render :new, locals: { recipe: @recipe } }
+     end
+   end
+ 
+   def create
+     @recipe = Recipe.new(post_params)
 
-    def show
-    end
-
-    def create
-    end
-
-    def new
-    end
-end
+     respond_to do |format|
+       format.html do
+         if @recipe.save
+           flash[:success] = 'Recipe created successfully'
+           redirect_to recipes_url
+         else
+           flash.now[:error] = 'Error: Recipe could not be created'
+           render :new, locals: { recipe: @recipe }
+         end
+       end
+     end
+   end
+ 
+   def destroy
+     Recipe.find(params[:id]).delete
+   end
+ 
+   private
+ 
+   def post_params
+     post_params = params.require(:recipe).permit(:name, :prep_time, :cooking_time, :description, :public)
+     post_params[:user_id] = current_user.id
+     post_params
+   end
+ end
