e991d7f4f452c2a909aad8a92c5a0f71cbae0c72
braincee <kwesi938@gmail.com>
2022-11-09 20:04:25 +0000

    Add general shopping list controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,76 +1,74 @@
-# frozen_string_literal: true
-
 User.create!([
-               {
-                 name: 'Steve',
-                 email: 'steve@gmail.com',
-                 password: '123456'
-               },
-               {
-                 name: 'Daniel',
-                 email: 'daniel@gmail.com',
-                 password: '123456'
-               }
-             ])
+  {
+    name: 'Steve',
+    email: 'steve@gmail.com',
+    password: '123456'
+  },
+  {
+    name: 'Daniel',
+    email: 'daniel@gmail.com',
+    password: '123456'
+  }
+])
 
 Food.create!([
-               {
-                 user_id: 1,
-                 name: 'Apple',
-                 measurement_unit: 'grams',
-                 unit_price: 10
-               },
-               {
-                 user_id: 1,
-                 name: 'Orange',
-                 measurement_unit: 'grams',
-                 unit_price: 3
-               },
-               {
-                 user_id: 2,
-                 name: 'Almond',
-                 measurement_unit: 'grams',
-                 unit_price: 12
-               },
-               {
-                 user_id: 2,
-                 name: 'Resins',
-                 measurement_unit: 'grams',
-                 unit_price: 22
-               }
-             ])
+  {
+    user_id: 1,
+    name: 'Rice',
+    measurement_unit: 'kg',
+    unit_price: 10
+  },
+  {
+      user_id: 1,
+      name: 'Beans',
+      measurement_unit: 'kg',
+      unit_price: 45
+  },
+  {
+    user_id: 2,
+    name: 'Bread',
+    measurement_unit: 'kg',
+    unit_price: 20
+  },
+  {
+    user_id: 2,
+    name: 'Cake',
+    measurement_unit: 'kg',
+    unit_price: 17
+  },
+])
 
 Recipe.create!([
-                 {
-                   user_id: 2,
-                   name: 'Loslas',
-                   preparation_time: '2hrs',
-                   cooking_time: '25min',
-                   description: 'Loslas descrption',
-                   public: true
-                 },
-                 {
-                   user_id: 1,
-                   name: 'Creapas',
-                   preparation_time: '1hrs',
-                   cooking_time: '45min',
-                   description: 'Creapas descrption',
-                   public: false
-                 },
-                 {
-                   user_id: 1,
-                   name: 'Dasus',
-                   preparation_time: '3hrs',
-                   cooking_time: '1.5hrs',
-                   description: 'Dasus descrption',
-                   public: true
-                 }, {
-                   user_id: 2,
-                   name: 'Pilost',
-                   preparation_time: '7hrs',
-                   cooking_time: '3hrs',
-                   description: 'Pilost descrption',
-                   public: false
-                 }
-               ])
+  {
+    user_id: 2,
+    name: 'Kerala Chicken roast',
+    preparation_time: '2hrs',
+    cooking_time: '25min',
+    description: 'Kerala Chicken descrption',
+    public: true
+  },
+  {
+    user_id: 1,
+    name: 'Grilled Cheese',
+    preparation_time: '1hrs',
+    cooking_time: '45min',
+    description: 'Grilled Cheese descrption',
+    public: true
+  },
+  {
+    user_id: 1,
+    name: 'Fried Chicken',
+    preparation_time: '1 hour',
+    cooking_time: '45 minutes',
+    description: 'Fried Chicken descrption',
+    public: true
+  },{
+    user_id: 2,
+    name: 'Jollof Rice',
+    preparation_time: '10 mins',
+    cooking_time: '45 mins',
+    description: 'Jollof Rice descrption',
+    public: true
+  }
+])
 puts 'Seed data loaded successfully'
