d7ade9e8a572b6a5337d99836d8abc04a0ef2f7a
braincee <kwesi938@gmail.com>
2022-11-09 02:25:04 +0000

    add public recipes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  get 'public_recipes/index'
   get 'recipes/index'
   devise_for :users
-  resources :recipes, only: [:index, :show, :new, :create, :destroy, :update] do
-    resources :food_recipes, only: [:new, :create, :destroy, :edit, :update]
-  end
+  resources :recipes, only: [:index, :new, :create, :destroy]
   root 'users#index'
   resources :foods
 end
