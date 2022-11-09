14ef530105787a54fc6e44483dee3b7fa9420e53
braincee <kwesi938@gmail.com>
2022-11-09 02:27:46 +0000

    Add public recipe controller

---
+++ app/controllers/public_recipes_controller.rb
@@ -0,0 +1,6 @@
+class PublicRecipesController < ApplicationController
+    def index
+      @public_recipes = Recipe.includes(:food_recipes, :user).where('public = true').order(id: :desc)
+    end
+  end
+  
