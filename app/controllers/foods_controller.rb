63f2b973967aa09133862c9119f2c452911cb4c2
benshidanny11 <benshidanny11@gmail.com>
2022-11-09 20:51:23 +0200

    Resolve linter errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,35 +1,36 @@
 class FoodsController < ApplicationController
   def index
-    @foods=Food.all
- end
+    @foods = Food.all
+  end
 
-def show
-    @food=Food.find(params[:id])
-end
+  def show
+    @food = Food.find(params[:id])
+  end
 
-def destroy
-  @food=Food.find(params[:id])
-  if @food.destroy
+  def destroy
+    @food = Food.find(params[:id])
+    return unless @food.destroy
+
     redirect_to foods_path
   end
-end
 
-def create
-    @food=Food.new(food_data)
-    @food.user=User.find(current_user.id)
+  def create
+    @food = Food.new(food_data)
+    @food.user = User.find(current_user.id)
     if @food.save
-        redirect_to food_path(@food)
-   else 
-    redirect_to root_path
-   end  
-end
+      redirect_to food_path(@food)
+    else
+      redirect_to root_path
+    end
+  end
 
-def new
-  @food=Food.new
-end
+  def new
+    @food = Food.new
+  end
 
-private
-def food_data
-    food_data=params.require(:food).permit(:name, :measurement_unit, :unit_price, :quantity)
+  private
+
+  def food_data
+    params.require(:food).permit(:name, :measurement_unit, :unit_price, :quantity)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'public_recipes/index'
   get 'recipes/index'
   devise_for :users
-  resources :recipes, only: [:index, :show, :new, :create, :destroy, :update] do
-    resources :food_recipes, only: [:new, :create, :destroy, :edit, :update]
+  resources :recipes, only: %i[index show new create destroy update] do
+    resources :food_recipes, only: %i[new create destroy edit update]
   end
   root 'users#index'
   resources :foods
