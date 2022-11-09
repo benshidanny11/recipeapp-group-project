b69738e863cc2e6963b12781ab5800664d0a949c
braincee <kwesi938@gmail.com>
2022-11-09 01:58:56 +0000

    Add food recipe resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
-# Rails.application.routes.draw do
-#   get 'recipes/index'
-#   devise_for :users
-#   resources :recipes, only: [:index, :show, :new, :create, :destroy, :update] do
-#     resources :food_recipes, only: [:new, :create, :destroy, :edit, :update]
-#   end
-#   root 'users#index'
-#   resources :foods
-# end
+Rails.application.routes.draw do
+  get 'recipes/index'
+  devise_for :users
+  resources :recipes, only: [:index, :show, :new, :create, :destroy, :update] do
+    resources :food_recipes, only: [:new, :create, :destroy, :edit, :update]
+  end
+  root 'users#index'
+  resources :foods
+end
