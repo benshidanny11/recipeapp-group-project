8a36a4eafe144f12583bd40c357bf476fe05f00b
braincee <kwesi938@gmail.com>
2022-11-10 08:56:04 +0000

    Add test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'User model' do
-    subject { User.new(name: 'Steve', email: 'steve@example.com', password: '123456') }
+    subject { User.new(name: 'Steve', email: 'steve@example.com', password: '12345') }
     before { subject.save }
 
     it 'check the name is not blank' do
